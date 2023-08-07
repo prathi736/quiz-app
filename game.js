cca382b33d4cd1c9096b0a8f2218e59442a91c10
Pulkit Rathi <pulkitr2002@gmail.com>
2023-08-07 17:04:35 +0530

    made changes

--- game.js
+++ game.js
@@ -3,6 +3,9 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
+
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -10,22 +13,22 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [ ];
+let questions = [];
 
-fetch(
-    'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
-)
-    .then((res) => {
+fetch('https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple')
+    .then( res => {
+        console.log(res);
         return res.json();
     })
-    .then((loadedQuestions) => {
-        questions = loadedQuestions.results.map((loadedQuestion) => {
+    .then(loadedQuestions => {
+        console.log(loadedQuestions.results);
+        questions = loadedQuestions.results.map(loadedQuestion => {
             const formattedQuestion = {
-                question: loadedQuestion.question,
+                question: loadedQuestion.question
             };
 
             const answerChoices = [...loadedQuestion.incorrect_answers];
-            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
             answerChoices.splice(
                 formattedQuestion.answer - 1,
                 0,
@@ -33,16 +36,16 @@ fetch(
             );
 
             answerChoices.forEach((choice, index) => {
-                formattedQuestion['choice' + (index + 1)] = choice;
+                formattedQuestion["choice" + (index + 1)] = choice;
             });
-
             return formattedQuestion;
         });
+
         startGame();
     })
-    .catch((err) => {
+    .catch( err => {
         console.error(err);
-    });
+    })
 
 // Constants
 const CORRECT_BONUS = 10;
@@ -53,6 +56,8 @@ startGame = () => {
   score = 0;
   availableQuestions = [...questions];
   getNewQuestions();
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
 };
 
 getNewQuestions = () => {
@@ -117,4 +122,3 @@ incrementScore = (num) => {
   scoreText.innerText = score;
 };
 
-startGame();
